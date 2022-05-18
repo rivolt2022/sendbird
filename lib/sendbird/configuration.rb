ea16a5072963eade22e1bdb72646e990755eb73d
rivolt2022 <97595524+rivolt2022@users.noreply.github.com>
2022-05-18 09:34:21 +0900

    Update configuration.rb

--- lib/sendbird/configuration.rb
+++ lib/sendbird/configuration.rb
@@ -2,7 +2,7 @@ module Sendbird
   module Configuration
     PUBLIC_METHODS = [:applications, :user, :password, :default_app]
 
-    SENDBIRD_ENDPOINT = https://api-629F342C-EA42-4A41-B41D-93E938B9E4EA.sendbird.com/v3/'
+    SENDBIRD_ENDPOINT = 'https://api-629F342C-EA42-4A41-B41D-93E938B9E4EA.sendbird.com/v3/'
 
     attr_accessor *PUBLIC_METHODS
 
